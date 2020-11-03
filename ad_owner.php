0657dcd19bb3390167ae7d0d6cc7766afeaa9ac4
Shouryamup <mshourya9@gmail.com>
2020-11-03 14:34:43 +0530

    added owner php files

--- ad_owner.php
+++ ad_owner.php
@@ -1,3 +1,32 @@
+<?php
+session_start();
+error_reporting(0);
+include("config.php");
+if(isset($_POST['submit']))
+{
+$ret=mysqli_query($con,"SELECT * FROM admin_table WHERE ad_username='".$_POST['username']."' and ad_password='".$_POST['password']."'");
+$num=mysqli_fetch_array($ret);
+if($num>0)
+{
+$extra="admin.php";//
+$_SESSION['login']=$_POST['username'];
+$_SESSION['id']=$num['ad_id'];
+$host=$_SERVER['HTTP_HOST'];
+$uri=rtrim(dirname($_SERVER['PHP_SELF']),'/\\');
+header("location:http://$host$uri/$extra");
+exit();
+}
+else
+{
+$_SESSION['errmsg']="Invalid username or password";
+$extra="business_em_ad.php";
+$host  = $_SERVER['HTTP_HOST'];
+$uri  = rtrim(dirname($_SERVER['PHP_SELF']),'/\\');
+header("location:http://$host$uri/$extra");
+exit();
+}
+}
+?>
 <!DOCTYPE html>
 <html lang="en">
 	<head>
@@ -19,8 +48,10 @@
 		<link rel="stylesheet" href="assets/css/plugins.css">
 		<link rel="stylesheet" href="assets/css/themes/theme-1.css" id="skin_color" />
 	</head>
-	<body class="login">
-		<div class="row">
+
+        
+
+        <div class="row">
 			<div class="main-login col-xs-10 col-xs-offset-1 col-sm-8 col-sm-offset-2 col-md-4 col-md-offset-4">
 				<div class="logo margin-top-30">
 				<h2>Admin Login</h2>
@@ -34,7 +65,7 @@
 							</legend>
 							<p>
 								Please enter your name and password to log in.<br />
-								<span style="color:red;"></span>
+								<span style="color:red;"><?php echo htmlentities($_SESSION['errmsg']); ?><?php echo htmlentities($_SESSION['errmsg']="");?></span>
 							</p>
 							<div class="form-group">
 								<span class="input-icon">
