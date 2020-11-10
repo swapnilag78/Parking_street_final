086db0922004a5c65e75c506efa765a36d6b663d
TARUSSI <tarussisingh@gmail.com>
2020-11-10 23:48:51 +0530

    index page UI changed

--- index.php
+++ index.php
@@ -3,13 +3,32 @@
 
 <head>
   <meta charset="UTF-8">
+  <link rel="stylesheet" type="text/css" href="css/style_1.css">
+  <link rel="stylesheet" type="text/css" href="css/style.css">
+  <link rel="stylesheet" type="text/css" href="css/style_2.css">
+  <link href='http://fonts.googleapis.com/css?family=Ropa+Sans' rel='stylesheet' type='text/css'>
+		<script src="http://ajax.googleapis.com/ajax/libs/jquery/1.8.3/jquery.min.js"></script>
+		<script src="js/responsiveslides.min.js"></script>
+  <link href="css/style.css" rel="stylesheet" type="text/css"  media="all" />
+  <link rel="stylesheet" href="css/responsiveslides.css">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>Parking Street</title>
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
+  
   <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
-  <link rel="stylesheet" type="text/css" href="css/style.css">
-  <link rel="stylesheet" type="text/css" href="css/style_1.css">
+
+		  <script>
+		    // You can also use "$(window).load(function() {"
+			    $(function () {
+			
+			      // Slideshow 1
+			      $("#slider1").responsiveSlides({
+			        maxwidth: 1600,
+			        speed: 600
+			      });
+			});
+		  </script>
 
 </head>
 
@@ -38,24 +57,17 @@
           <a class="nav-link" href="#">Contact Us</a>
         </li>
         <a href="hotel_regis.php"> <button type="button" class="btn btn-primary">Register Now with Us</button></a>
+        <div>
+    </div>
       </ul>
     </div>
   </nav>
-
+    </div>
+ 
   <div id="demo" class="carousel slide" data-ride="carousel">
     <ul class="carousel-indicators">
       <li data-target="#demo" data-slide-to="0" class="active"></li>
-
-
     </ul>
-    <div class="carousel-inner">
-      <div class="carousel-item active">
-        <img src="images/car.jpg" alt="Los Angeles" width="1100" height="500">
-        <div class="carousel-caption">
-          <h3>Welcome to Parking Street</h3>
-          <p>A unique parking experience for your car!!</p>
-        </div>
-      </div>
 
     </div>
     <a class="carousel-control-prev" href="#demo" data-slide="prev">
@@ -66,13 +78,7 @@
     </a>
   </div>
 
-  <span class="border">
-    <section class="my-5">
-      <div class="mr-5 mx-5">
-        <h1> Why Choose our Platform?</h1>
-      </div>
-    </section>
-  </span>
+ 
 
   <div class="content-grids">
     <div class="wrap">
@@ -85,7 +91,7 @@
           </div>
           <div class="text list_1_of_2">
             <h3>Customers</h3>
-            <p>Register & Book Appointment</p>
+            <p><b>Register Now!</b></p>
             <div class="button"><span><a href="customer_signin.php">Click Here</a></span></div>
           </div>
         </div>
@@ -95,8 +101,8 @@
             <img src="images/grid-img1.png">
           </div>
           <div class="text list_1_of_2">
-            <h3>Hotel Login</h3>
-
+            <h3>Business Login</h3>
+            <p><b> Already have an account?</b> </p>
             <div class="button"><span><a href="business_em_ad.php">Click Here</a></span></div>
           </div>
         </div>
@@ -106,14 +112,42 @@
             <img src="images/grid-img2.png">
           </div>
           <div class="text list_1_of_2">
-            <h3>Admin Login <br> (Owners)</h3>
-
+            <h3>Admin Login</h3>
+            <h5>[Owners]</h5>
             <div class="button"><span><a href="ad_owner.php">Click Here</a></span></div>
           </div>
         </div>
       </div>
     </div>
   </div>
+  <div class="clear"> </div>
+			<!--start-image-slider---->
+					<div class="image-slider">
+						<!-- Slideshow 1 -->
+					    <ul class="rslides">
+					      <li><img src="images/background-pic-11.jpg" alt=""></li>
+					      <li><img src="images/car3.jpg" alt=""></li>
+					      <li><img src="images/Parking Lot.jpg" alt=""></li>
+					    </ul>
+						 <!-- Slideshow 2 -->
+					</div>
+					<!--End-image-slider---->
+		    <div class="clear"> </div>
+
+  <span class="border">
+    <section class="my-5">
+      <div class="mr-5 mx-5">
+        <h1> Why Choose our Platform?</h1>
+        <div class="why-us">
+          <p></p>
+        </div>
+      </div>
+    </section>
+  </span>
+
+  <!--Footer -->
+<?php include('footer.php');?>
+<!-- /Footer--> 
 
   <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
   <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
