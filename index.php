d2dada0414fa44bd94141faca79e0140cf8f4972
TARUSSI <tarussisingh@gmail.com>
2020-11-15 17:03:56 +0530

    Index changes

--- index.php
+++ index.php
@@ -4,46 +4,31 @@
 <head>
   <meta charset="UTF-8">
   <link rel="stylesheet" type="text/css" href="css/style_1.css">
-  <link rel="stylesheet" type="text/css" href="css/style.css">
+  <link rel="stylesheet" type="text/css" href="css/style.css" media="all">
   <link rel="stylesheet" type="text/css" href="css/style_2.css">
   <link href='http://fonts.googleapis.com/css?family=Ropa+Sans' rel='stylesheet' type='text/css'>
   <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.8.3/jquery.min.js"></script>
   <script src="js/responsiveslides.min.js"></script>
-  <link href="css/style.css" rel="stylesheet" type="text/css" media="all" />
-  <link rel="stylesheet" href="css/responsiveslides.css">
+    <link rel="stylesheet" href="css/responsiveslides.css">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>Parking Street</title>
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
-<<<<<<< HEAD
-=======
   <link rel="stylesheet" type="text/css" href="css/style_1.css">
   <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
   <link rel="stylesheet" type="text/css" href="css/style.css">
 
->>>>>>> 739a7de608fad4281cceb910dee74247fd9b3503
   <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
 
-  <script>
-    // You can also use "$(window).load(function() {"
-    $(function() {
-
-      // Slideshow 1
-      $("#slider1").responsiveSlides({
-        maxwidth: 1600,
-        speed: 600
-      });
-    });
-  </script>
 
 
 </head>
 
 <body>
   <!--------navigation bar---------------->
-  <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+  <nav class="navbar navbar-expand-lg navbar-light bg-light">
     <a class="navbar-brand" href="#">
-      <img src="images/final_logo.jpg" width="60" height="50" class="d-inline-block align-top" alt="" loading="lazy">
+      <img src="images/final_logo.jpg" width="60" height="50" class="d-inline-block align-top" alt="LOGO" loading="lazy">
     </a>
     <a class="navbar-brand" href="#">Parking Street</a>
     <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
@@ -65,21 +50,13 @@
     </div>
   </nav>
 
-  <div id="demo" class="carousel slide" data-ride="carousel">
-    <ul class="carousel-indicators">
-      <li data-target="#demo" data-slide-to="0" class="active"></li>
-    </ul>
 
+
+
+  <!--image---->
+  <div class="gallery">
+      <li><img src="images/photo1.jpg" alt="image" ></li>
   </div>
-  <a class="carousel-control-prev" href="#demo" data-slide="prev">
-    <span class="carousel-control-prev-icon"></span>
-  </a>
-  <a class="carousel-control-next" href="#demo" data-slide="next">
-    <span class="carousel-control-next-icon"></span>
-  </a>
-  </div>
-
-
 
   <div class="content-grids">
     <div class="wrap">
@@ -134,19 +111,7 @@
       </div>
     </div>
   </div>
-  <div class="clear"> </div>
-  <!--start-image-slider---->
-  <div class="image-slider">
-    <!-- Slideshow 1 -->
-    <ul class="rslides">
-      <li><img src="images/background-pic-11.jpg" alt=""></li>
-      <li><img src="images/car3.jpg" alt=""></li>
-      <li><img src="images/Parking Lot.jpg" alt=""></li>
-    </ul>
-    <!-- Slideshow 2 -->
-  </div>
-  <!--End-image-slider---->
-  <div class="clear"> </div>
+
 
   <span class="border">
     <section class="my-5">
